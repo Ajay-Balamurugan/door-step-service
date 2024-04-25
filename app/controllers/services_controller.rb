45a5e60c8a4be134e7ffe3f3efbd4ea63c2be52e
Ajay Balamurugan <121103098+Ajay-Balamurugan@users.noreply.github.com>
2024-04-25 21:36:12 +0530

    Implement AJAX for creating services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,20 +1,22 @@
 class ServicesController < ApplicationController
     def index
-        @services = Service.all
+      @service = Service.new
+      @services = Service.all
     end
     
     def new
        @service = Service.new
     end
  
-    def create
-       @service = Service.new(service_params)
-       if @service.save
-          redirect_to admin_dashboard_path, notice: "Successfully Created Service"
-       else
-          render :new
-       end
-    end
+
+   def create
+      @service = Service.new(service_params)
+      if @service.save
+         render partial: 'service', locals: { service: @service }, status: :created
+      else
+         render json: { message: 'Unable to Create Service.' }, status: :unprocessable_entity
+      end
+   end
  
     def edit
        @service = Service.find(params[:id])
@@ -28,11 +30,17 @@ class ServicesController < ApplicationController
           render :edit
        end
     end
+
+    def destroy
+      @service = Service.find(params[:id])
+      @service.destroy
+      redirect_to services_path, notice: "Successfully deleted services"
+    end
  
     private
  
     def service_params
        params.require(:service).permit(:title, :description, images: [])
     end
- 
+
 end
