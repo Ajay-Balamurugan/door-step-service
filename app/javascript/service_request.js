e3e866fed801b967a3dbc185f2ea7ed04670f8dc
Ajay Balamurugan <abalamurugan@C02FF51DMD6M.local>
2024-05-07 17:20:51 +0530

    Add CSS for employee and admin pages

--- app/javascript/service_request.js
+++ app/javascript/service_request.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
   //AJAX for displaying order placed message
-  $("#place_order_btn").on("click", function (event) {
-    event.preventDefault();
+  $("#place_order_btn")
+    .off("click")
+    .on("click", function (event) {
+      event.preventDefault();
 
-    $.ajax({
-      method: "POST",
-      url: "/service_requests/",
-      success: function (response) {
-        $("#cart_items_container").remove();
-        $(".cart_container").append(response);
-      },
-      error: function (error) {
-        console.error("Unable to place order", error);
-      },
+      $.ajax({
+        method: "POST",
+        url: "/service_requests/",
+        success: function (response) {
+          $("#cart_items_container").remove();
+          $(".cart_container").append(response);
+        },
+        error: function (error) {
+          console.error("Unable to place order", error);
+        },
+      });
     });
-  });
 });
